bbd78652230e95e8c72f0ac05d7734e8bc38d5d6
rustem <kaidohmary@gmail.com>
2023-05-16 23:05:10 +0300

    add class Video

---
+++ src/video.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from googleapiclient.discovery import build
+
+
+# Как правильно импортировать и применять библиотеку dotenv?
+load_dotenv(find_dotenv())
+
+
+class Video:
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id: str) -> None:
+        self.video_id = video_id
+        self.title = self.video_title()
+        self.url_video = f'https://www.youtube.com/watch?v={self.video_id}'
+        self.view_count = self.view_count()
+        self.likes_count = self.likes_count()
+
+    def __str__(self) -> str:
+        return self.title
+
+    def _video_stat(self):
+        return self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                          id=self.video_id
+                                          ).execute()
+
+    def video_title(self):
+        stat = self._video_stat()
+        return stat['items'][0]['snippet']['title']
+
+    def view_count(self):
+        stat = self._video_stat()
+        return stat['items'][0]['statistics']['viewCount']
+
+    def likes_count(self):
+        stat = self._video_stat()
+        return stat['items'][0]['statistics']['likeCount']
+
+
+v = Video('9lO06Zxhu88')
+print(v.title)
+print(v.view_count)
+print(v.likes_count)
+print(v._video_stat())
+print(v.url_video)
